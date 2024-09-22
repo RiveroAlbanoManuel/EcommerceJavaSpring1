0b2617efc2514d733b8269ca5848c7acfa0a1dac
Manuel Rivero Albano <42250146+RiveroAlbanoManuel@users.noreply.github.com>
2024-09-22 18:56:04 -0300

    Carga Inicial.

--- src/main/java/com/ecommerce/ecommerce/model/DetalleOrden.java
+++ src/main/java/com/ecommerce/ecommerce/model/DetalleOrden.java
@@ -12,7 +12,7 @@ public class DetalleOrden {
     public DetalleOrden() {
     }
 
-    
+    //comment
     
     public DetalleOrden(Integer id, String nombre, double cantidad, double precio, double total) {
         this.id = id;
